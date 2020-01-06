a18e69adb1a8359fc6d3b4beeee5da89880635c3
Russell <rgee258@gmail.com>
2020-01-05 23:48:17 -0500

    Set up routes, basic views, and GET controller methods

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,41 +1,61 @@
-let category = require('../models/category.js');
+let Category = require('../models/category.js');
 
 // Category GET index
-export.category_list = function(req, res) {
-  res.render('categories', { title: 'All Categories' });
+exports.category_list = function(req, res, next) {
+  Category
+    .find({}, 'name url')
+    .exec(function(err, all_categories) {
+      if (err) { return next(err); }
+      res.render('categories', { title: 'All Categories', category_list: all_categories });
+    });
 };
 
 // Category GET display
-export.category_display = function(req, res) {
-  res.render('category', { title: 'View Category'});
+exports.category_display = function(req, res, next) {
+  Category
+    .findById(req.params.id)
+    .exec(function(err, category) {
+      if (err) { return next(err); }
+      res.render('category', { title: category.name, category_display: category});
+    });
 };
 
 // Category GET new form
-export.category_new_form = function(req, res) {
-
+exports.category_new_form = function(req, res, next) {
+  res.render('category_new', { title: 'New Category' });
 };
 
 // Category POST new form
-export.category_new_submit = function(req, res) {
+exports.category_new_submit = function(req, res, next) {
 
 };
 
 // Category GET update form
-export.category_update_form = function(req, res) {
-
+exports.category_update_form = function(req, res, next) {
+  Category
+    .findById(req.params.id)
+    .exec(function(err, category) {
+      if (err) { return next(err); }
+      res.render('category_update', { title: 'Update Category', current_category: category });
+    });
 };
 
 // Category POST update form
-export.category_update_submit = function(req, res) {
+exports.category_update_submit = function(req, res, next) {
 
 };
 
 // Category GET delete form
-export.category_delete_form = function(req, res) {
-
+exports.category_delete_form = function(req, res, next) {
+  Category
+    .findById(req.params.id, 'id')
+    .exec(function(err, category) {
+      if (err) { return next(err); }
+      res.render('category_delete', { title: 'Delete Category', current_category: category });
+    });
 };
 
 // Category POST delete form
-export.category_delete_submit = function(req, res) {
+exports.category_delete_submit = function(req, res, next) {
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
+
+let Item = require('../models/item');
+let Category = require('../models/category');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+
+  async.parallel({
+    // Idea of approaching referencing an object by id:
+    //   https://stackoverflow.com/questions/36201689/mongoose-find-a-document-by-reference-property
+    // Then query chaining using prior results, see second answer:
+    //   https://stackoverflow.com/questions/20699947/how-to-return-query-results-to-a-variable-using-mongoose/20702542
+    fresh_water_count: function(callback) {
+      Category
+        .findOne({ name: 'Fresh Water' }, '_id')
+        .exec(function(err, category) {
+          Item.countDocuments({ 'category': category.id }, callback);
+        });
+    },
+    salt_water_count: function(callback) {
+      Category
+        .findOne({ name: 'Salt Water' }, '_id')
+        .exec(function(err, category) {
+          Item.countDocuments({ 'category': category.id }, callback);
+        });
+    },
+  }, function(err, results) {
+    res.render('index', { title: 'Express', error: err, data: results });
+  });
 });
 
 module.exports = router;
