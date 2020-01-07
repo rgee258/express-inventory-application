081675c757aaa1e354913f5fb8de3bb7f98d9280
rgee258 <9219093+rgee258@users.noreply.github.com>
2020-01-07 00:53:46 -0500

    Added POST handling for item new and update routes

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,6 @@
 let Item = require('../models/item.js');
 let Category = require('../models/category.js');
+const validator = require('express-validator');
 
 // Item GET index
 exports.item_list = function(req, res, next) {
@@ -16,9 +17,9 @@ exports.item_display = function(req, res, next) {
   Item
     .findById(req.params.id)
     .populate('category')
-    .exec(function(err, item) {
+    .exec(function(err, current_item) {
       if (err) { return next(err); }
-      res.render('item', { title: item.name, item_display: item});
+      res.render('item', { title: current_item.name, item_display: current_item });
     });
 };
 
@@ -33,9 +34,50 @@ exports.item_new_form = function(req, res, next) {
 };
 
 // Item POST new form
-exports.item_new_submit = function(req, res, next) {
+exports.item_new_submit = [
 
-};
+  // Field validation
+  validator.body('item_name', 'Item name is required.').isLength({ min: 1 }).trim(),
+  validator.body('item_description', 'Item description is required.').isLength({ min: 1 }).trim(),
+  validator.body('item_category', 'Item must be associated with a provided category.').isLength({ min: 1 }).trim(),
+  validator.body('item_price', 'Item is required to have a proper currency format.').isCurrency(),
+  validator.body('item_stock', 'Item is required to contain a stock count.').isInt(),
+
+  validator.sanitizeBody('*').escape(),
+
+  (req, res, next) => {
+
+    // Retrieve validation errors
+    const errors = validator.validationResult(req);
+
+    // Create new item with provided fields
+    let item = new Item(
+      {
+        name: req.body.item_name,
+        description: req.body.item_description,
+        category: req.body.item_category,
+        price: req.body.item_price,
+        stock: req.body.item_stock
+      }
+    );
+
+    // Handle error by rendering form again with errors
+    if (!errors.isEmpty()) {
+      Category
+        .find({}, 'name id')
+        .exec(function(err, categories) {
+          if (err) { return next(err); }
+          res.render('item_new', { title: 'New Item', category_list: categories, errors: errors.array() });
+        });
+    } else {
+      item.save(function(err) {
+        if (err) { return next(err); }
+        // Redirect to the new item on success
+        res.redirect(item.url);
+      });
+    }
+  }
+];
 
 // Item GET update form
 exports.item_update_form = function(req, res, next) {
@@ -52,9 +94,53 @@ exports.item_update_form = function(req, res, next) {
 };
 
 // Item POST update form
-exports.item_update_submit = function(req, res, next) {
+exports.item_update_submit = [
 
-};
+  // Field validation
+  validator.body('item_name', 'Item name is required.').isLength({ min: 1 }).trim(),
+  validator.body('item_description', 'Item description is required.').isLength({ min: 1 }).trim(),
+  validator.body('item_category', 'Item must be associated with a provided category.').isLength({ min: 1 }).trim(),
+  validator.body('item_price', 'Item is required to have a proper currency format.').isCurrency(),
+  validator.body('item_stock', 'Item is required to contain a numerical stock count.').isInt(),
+
+  validator.sanitizeBody('*').escape(),
+
+  (req, res, next) => {
+
+    // Retrieve validation errors
+    const errors = validator.validationResult(req);
+
+    // Create new item with provided fields
+    let item = new Item(
+      {
+        name: req.body.item_name,
+        description: req.body.item_description,
+        category: req.body.item_category,
+        price: req.body.item_price,
+        stock: req.body.item_stock,
+        // Here you need to include the id in params or a new one will be created
+        _id: req.params.id
+      }
+    );
+
+    // Handle error by rendering form again with errors
+    if (!errors.isEmpty()) {
+      Category
+        .find({}, 'name id')
+        .exec(function(err, categories) {
+          if (err) { return next(err); }
+          res.render('item_update', { title: 'Update Item', category_list: categories, current_item: item, errors: errors.array() });
+        });
+    } else {
+      // Unlike saving a new object, here we call findByIdAndUpdate on the model and pass in the item
+      Item.findByIdAndUpdate(req.params.id, item, function(err, updatedItem) {
+        if (err) { return next(err); }
+        // Redirect to the new item on success
+        res.redirect(updatedItem.url);
+      });
+    }
+  }
+];
 
 // Item GET delete form
 exports.item_delete_form = function(req, res, next) {
